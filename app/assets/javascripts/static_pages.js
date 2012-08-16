5392a15782d2207b470c842dcc5038c3f91d2a10
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-16 12:00:11 -0400

    Sounds Sounds now play in chrome and on the iPad.

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,30 @@
+var audio = {};
+
+$(document).ready(function() {
+//    audio.element = document.createElement('audio');
+//    audio.element.src = Modernizr.audio.ogg ? "/assets/" + audio_id + ".ogg" :
+//                        Modernizr.audio.mp3 ? "/assets/" + audio_id + ".mp3" :
+//                                              "/assets/" + audio+id + ".m4a";
+//
+    $("#stones img").mouseenter(function() {
+        audio_id = jQuery.attr(this, "data-sound")
+        e = $("#"+ audio_id)[0];
+        if(Modernizr.audio && e != undefined) {
+            e.src = Modernizr.audio.ogg ? "/assets/" + audio_id + ".ogg" :
+                    Modernizr.audio.mp3 ? "/assets/" + audio_id + ".mp3" :
+                                          "/assets/" + audio+id + ".m4a";
+            e.play();
+        }
+    });
+
+    $("#stones img").click(function() {
+        audio_id = jQuery.attr(this, "data-sound")
+        e = $("#"+ audio_id)[0];
+        if(Modernizr.audio && e != undefined) {
+            e.src = Modernizr.audio.ogg ? "/assets/" + audio_id + ".ogg" :
+                    Modernizr.audio.mp3 ? "/assets/" + audio_id + ".mp3" :
+                                          "/assets/" + audio+id + ".m4a";
+            e.play();
+        }
+    });
+});
--- app/assets/javascripts/static_pages.js.coffee
+++
@@ -1,7 +0,0 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-$(".stone").mouseenter((e)-> 
-    audio_id = e.srcElement.getAttribute("data-sound")
-    audio = $("#" + audio_id)[0]
-    audio.play())
